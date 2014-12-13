7ce1160a39c04bf1883e4440bd4bc71f25e7abaf
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-12-13 18:09:39 +0100

    Add Unit Class

--- AdvPT_Starcraft/Unit.h
+++ AdvPT_Starcraft/Unit.h
@@ -4,8 +4,27 @@
 class Unit :
     public Object
 {
+private:
+    UnitType _type;
+
 public:
-    Unit();
-    virtual ~Unit();
+    Unit(std::string unitName, Requirements &requirements)
+    {
+        this->_requirements = requirements;
+
+        // resolve unit type
+        for (unsigned int i = 0; i < _numberOfUnitsTypes; i++)
+        if (unitName == unitTypeStrings[i])
+        {
+            this->_type = static_cast<UnitType>(i);
+            return;
+        }
+    }
+    virtual ~Unit() {}
+
+    bool init()
+    {
+
+    }
 };
 
