7e8ed5e30d86e75ce6a1db92a4b17a90d84ae13e
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-12-14 23:08:25 +0100

    Change Types from Class to Namespace

--- AdvPT_Starcraft/Types.h
+++ AdvPT_Starcraft/Types.h
@@ -1,40 +1,29 @@
 #pragma once
 #include <string>
 
-class Types
+namespace Types
 {
-public:
-    Types() = delete;
-
     // enums
-    static enum ObjectType { Unit, Building, Unknown };
-    static const std::string objectTypeStrings[3];
+    enum ObjectType { Unit, Building, Unknown };
+    const std::string objectTypeStrings[3] = { "unit", "building", "unknown" };
 
-    static enum UnitType
+    enum UnitType
     {
         Probe, Zealot, Stalker, Sentry, Observer, Immortal, WarpPrism,
         Colossus, Phoenix, VoidRay, HighTemplar, DarkTemplar, Carrier, Mothership
     };
-    static const unsigned short _numberOfUnitsTypes = 14;
-    static const std::string unitTypeStrings[_numberOfUnitsTypes];
+    const unsigned short _numberOfUnitsTypes = 14;
+    const std::string unitTypeStrings[_numberOfUnitsTypes] = {
+        "probe", "zealot", "stalker", "sentry", "observer", "immortal", "warp_prism",
+        "colossus", "phoenix", "void_ray", "high_templar", "dark_templar", "carrier", "mothership" };
 
-    static enum BuildingType
+    enum BuildingType
     {
         Nexus, Pylon, Assimilator, Gateway, Warpgate, Forge, CyberneticsCore,
         PhotonCannon, Stargate, RoboticsFacility, TwilightControl, FleetBeacon, RoboticsBay, DarkShrine, TemplarArchives
     };
-    static const unsigned short _numberOfBuildingsTypes = 15;
-    static const std::string buildingTypeStrings[_numberOfBuildingsTypes];
+    const unsigned short _numberOfBuildingsTypes = 15;
+    const std::string buildingTypeStrings[_numberOfBuildingsTypes] = {
+        "nexus", "pylon", "assimilator", "gateway", "warpgate", "forge", "cybernetics_core",
+        "photon_cannon", "stargate", "robotics_facility", "twilight_control", "fleet_beacon", "robotics_bay", "dark_shrine", "templar_archives" };
 };
-
-
-// statics initializations
-const std::string Types::objectTypeStrings[3] = { "unit", "building", "unknown" };
-
-const std::string Types::unitTypeStrings[Types::_numberOfUnitsTypes] = {
-    "probe", "zealot", "stalker", "sentry", "observer", "immortal", "warp_prism",
-    "colossus", "phoenix", "void_ray", "high_templar", "dark_templar", "carrier", "mothership" };
-
-const std::string Types::buildingTypeStrings[Types::_numberOfBuildingsTypes] = {
-    "nexus", "pylon", "assimilator", "gateway", "warpgate", "forge", "cybernetics_core",
-    "photon_cannon", "stargate", "robotics_facility", "twilight_control", "fleet_beacon", "robotics_bay", "dark_shrine", "templar_archives" };
