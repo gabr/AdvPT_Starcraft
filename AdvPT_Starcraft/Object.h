ca150243dcfed76ec217c7695cb6161c61ee11e9
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-12-14 22:31:23 +0100

    init() method for Object

--- AdvPT_Starcraft/Object.h
+++ AdvPT_Starcraft/Object.h
@@ -28,11 +28,13 @@ protected:
         if (objectType == Types::Unit)
         {
             _name = Types::unitTypeStrings[specificType];
+            // todo : get _requirements from XmlReader
         }
 
         else if (objectType == Types::Building)
         {
             _name = Types::buildingTypeStrings[specificType];
+            // todo : get _requirements from XmlReader
         }
     }
 
@@ -70,6 +72,18 @@ public:
         return Object(Types::Unknown, -1);
     }
 
-    bool init() {};
+    // initialize object creatino
+    bool init(unsigned int currentTime, unsigned int& globalMineral, unsigned int &globalVespen, unsigned int &globalSupply)
+    {
+        // check requirements and decrement values if creating object is possible
+        if (!checkRequirements(globalMineral, globalVespen, globalSupply))
+            return false;
+
+        // starting object creation is possible
+        // set _startTime to currentTime and return success
+        _startTime = currentTime;
+        return true;
+    }
+
     std::string toString() { return _name; }
 };
