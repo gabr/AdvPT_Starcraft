c967f13878fd907fa25b52b0b6a960563633fe00
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-12-14 22:14:46 +0100

    createObject() static method and Object constructor

--- AdvPT_Starcraft/Object.h
+++ AdvPT_Starcraft/Object.h
@@ -5,6 +5,7 @@
 
 #include "Types.h"
 #include "Resources.h"
+#include "XmlReader.h"
 
 class Object
 {
@@ -15,11 +16,25 @@ protected:
 
     // object informations
     std::string _name = "";
+
     Types::ObjectType _type = Types::Unknown;
+    int _specificType = -1; // UnitType or BuildingType - depends on _type
+
     unsigned int _startTime = 0;
 
     // private constructor
-    Object(Types::ObjectType objectType) : _type(objectType) {};
+    Object(Types::ObjectType objectType, int specificType) : _type(objectType), _specificType(specificType)
+    {
+        if (objectType == Types::Unit)
+        {
+            _name = Types::unitTypeStrings[specificType];
+        }
+
+        else if (objectType == Types::Building)
+        {
+            _name = Types::buildingTypeStrings[specificType];
+        }
+    }
 
     // check requirements and decrement global 
     bool checkRequirements(unsigned int& globalMineral, unsigned int &globalVespen, unsigned int &globalSupply)
@@ -38,6 +53,23 @@ protected:
     }
 
 public:
-    virtual bool init() = 0; // <--- todo
+
+    static Object& createObject(const std::string objectName)
+    {
+        // try to find object in Units list
+        for (int i = 0; i < Types::_numberOfUnitsTypes; i++)
+            if (objectName == Types::unitTypeStrings[i])
+                return Object(Types::Unit, i);
+
+        // try to find object in Buildings list
+        for (int i = 0; i < Types::_numberOfBuildingsTypes; i++)
+            if (objectName == Types::buildingTypeStrings[i])
+                return Object(Types::Building, i);
+
+        // return Unknown object type
+        return Object(Types::Unknown, -1);
+    }
+
+    bool init() {};
     std::string toString() { return _name; }
 };
