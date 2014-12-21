98d57fbb9f14dc853a04468844760a98b5c441f2
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-12-21 20:27:02 +0100

    Init main

--- AdvPT_Starcraft/main.cpp
+++ AdvPT_Starcraft/main.cpp
@@ -4,9 +4,8 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Program: " << argv[0] << std::endl;
-    std::cout << CsvReader::updateUnits() << std::endl;
-    std::cout << CsvReader::updateBuildings() << std::endl;
+    // Getting files list
+
 
     return 0;
 }
